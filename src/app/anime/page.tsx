d5f3106c0a9df1296a3c22248f8da1116a97651f
Cookie2D <cookie50ds@gmail.com>
2025-04-05 01:06:03 +0300

    fix: pagination on query

--- src/app/anime/page.tsx
+++ src/app/anime/page.tsx
@@ -16,7 +16,7 @@ interface Props {
 
 const About: NextPage<Props> = async (props) => {
   const searchParams = await props.searchParams;
-  //const query = searchParams?.query || "";
+  const query = searchParams?.query || "";
   const category = searchParams?.category || "";
   const currentPage = Number(searchParams?.page) || 1;
 
@@ -26,13 +26,19 @@ const About: NextPage<Props> = async (props) => {
         <Search placeholder="Search anime..." />
       </div>
       <div className="my-8">
-        <Suspense key={category + currentPage} fallback={<AnimeListSkeleton />}>
+        <Suspense
+          key={category + currentPage + query}
+          fallback={<AnimeListSkeleton />}
+        >
           <AnimeList {...searchParams} currentPage={currentPage} />
         </Suspense>
       </div>
       <div>
-        <Suspense key={category + currentPage} fallback={<PaginationLoader />}>
-          <AnimeListPagination category={category} />
+        <Suspense
+          key={category + currentPage + query}
+          fallback={<PaginationLoader />}
+        >
+          <AnimeListPagination category={category} query={query} />
         </Suspense>
       </div>
     </>
